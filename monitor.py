3a8923a59819cd4b68b408b791031375bf7b558b
cy1701 <cy1701@gmail.com>
2017-01-25 20:05:23 +0000

    update

--- monitor.py
+++ monitor.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
+########################################################
+#
+# Monitor program to send switch status and temp to
+# MQTT proxy. 
+#
+#######################################################
+
 
 import os
 import time
 import subprocess
-#import onewire
-from w1thermsensor import W1ThermSensor
+from w1thermsensor import W1ThermSensor #https://github.com/timofurrer/w1thermsensor
 import paho.mqtt.client as mqtt
-import CHIP_IO.GPIO as GPIO
 
+import CHIP_IO.GPIO as GPIO
 GPIO.setup("CSID0", GPIO.IN)
 GPIO.setup("CSID1", GPIO.IN)
+
 mqttc = mqtt.Client()
 
 
 mqttc.username_pw_set(username="user", password="pass")
 
-
+# Testing GPIO 
 if GPIO.input("CSID0"):
     print("HIGH")
 else:
